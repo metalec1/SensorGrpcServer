f1f4f6d7ba9577bddbe1b7239697181cafc3998f
Deni <deni.cerovac@gmail.com>
2026-09-16 14:47:28 +0200

    Add Serilog writing to log file

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 using SensorGrpcServer.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 // Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddSingleton<SensorStateService>();
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
@@ -12,5 +20,7 @@ var app = builder.Build();
 //app.MapGrpcService<GreeterService>();
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+Log.Information("SensorGrpcServer sed zaganja");
 
 app.Run();
